c4a3f4f4891cc23e44626945c4e9614c1007c569
sajjad960 <sajjadhossainsb01@gmail.com>
2021-10-24 23:24:56 +0600

    after implementing upload feature

---
+++ README.md
@@ -0,0 +1,13 @@
+# GOOGLE DRIVE API
+
+## Instalation
+run this command
+```
+npm install
+```
+### Working with this
+You need your own account details for uploading file into your account drive.
+That's why you need to put all details into env file.
+
+I build this project for uploading my exported data from DB to the google drive.
+😅
---
+++ app.js
@@ -0,0 +1,47 @@
+const dotenv = require('dotenv')
+const {google} = require('googleapis');
+const path = require('path');
+const fs = require('fs')
+
+// import .env variables
+dotenv.config({path: "./config.env"});
+
+const REFRESH_TOKEN = '1//04HkkOBmbDq2ZCgYIARAAGAQSNwF-L9IrY6lzG6v_vByQ_M4jUgrMrDRqYzi793Xoq649Fhuf7hYNg1Cno7kPGz0wvxHjzmI_Rag'
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+);
+
+oauth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+
+const drive = google.drive({
+    version: 'v3',
+    auth: oauth2Client
+})
+
+const filePath = path.join(__dirname, 'uploadChecking.txt');
+
+async function uploadFile() {
+    try {
+        
+         const response = await drive.files.create({
+             requestBody: {
+                 name: 'uploadchecking.txt',
+                 mimeType: 'text/plain'
+             },
+             media: {
+                 mimeType: 'text/plain',
+                 body: fs.createReadStream(filePath)
+             }
+         })
+
+         console.log(response.data);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+uploadFile()
